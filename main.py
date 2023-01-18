08f1248a2286b113957a5388686dac7a97503399
KrzysztofKurowski0 <228528@student.uek.krakow.pl>
2023-01-19 00:41:28 +0100

    Added buttons to change the embed index in /characters

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import discord
 import genshin
 import asyncio
 from discord import app_commands
+from discord import ui
+from discord.ui import View
 from discord.ext import commands
 import json
 from replit import Database
 import genshinstats as gs
+import typing
+from MenuButtons import ButtonMenu
+
+
+
 
 class getgenshin():
     def stats(self,user):
@@ -70,7 +77,8 @@ async def profile(interaction: discord.Interaction):
     if str(interaction.user.id) in str(db["Users"]["User_ID"]).split("'"):
         stats = gg.stats(interaction.user.id)
         embed = discord.Embed(title=f"{interaction.user.name}'s Profile")
-        embed.set_thumbnail(url="https://pbs.twimg.com/media/E99MNCzVkAY-1V8?format=jpg&name=small")
+        embed.set_thumbnail(url=interaction.user.avatar.url)
+        embed.set_footer(icon_url = interaction.user.avatar.url ,text=f"Requested by {interaction.user.name}")
         await interaction.response.defer(ephemeral=False)
         await asyncio.sleep(10)
         for field, valuee in stats.items():
@@ -83,26 +91,21 @@ async def profile(interaction: discord.Interaction):
 async def characters(interaction: discord.Interaction):
     if str(interaction.user.id) in str(db["Users"]["User_ID"]).split("'"):
         characters = gg.characters(interaction.user.id)
-        embed = discord.Embed(title=f"{interaction.user.name}'s characters")
-        embed.set_thumbnail(url="https://pbs.twimg.com/media/E99MNCzVkAY-1V8?format=jpg&name=small")
-        await interaction.response.defer(ephemeral=False)
-        await asyncio.sleep(10)
-        j = 0
+        pages = [discord.Embed(title=f"{interaction.user.name}'s Characters"),discord.Embed(title=f"{interaction.user.name}'s Characters page 2")]
+        pages[0].set_thumbnail(url="https://pbs.twimg.com/media/E99MNCzVkAY-1V8?format=jpg&name=small")
+        pages[0].set_footer(icon_url = interaction.user.avatar.url ,text=f"Requested by {interaction.user.name}")
+        pages[1].set_thumbnail(url="https://pbs.twimg.com/media/E99MNCzVkAY-1V8?format=jpg&name=small")
+        pages[1].set_footer(icon_url = interaction.user.avatar.url ,text=f"Requested by {interaction.user.name}")
+        i=0
         for char in characters:
-            j += 1
-            if j % 25 != 0 and j<25:
-                embed.add_field(name=f"{char['name']:10}", value = f"{char['rarity']}* | lvl {char['level']:2} C{char['constellation']}", inline=True)
-                
+            if i<24:
+                i+=1
+                pages[0].add_field(name=f"{char['name']:10}", value = f"{char['rarity']}* | lvl {char['level']:2} C{char['constellation']}", inline=True)
             else:
-                if j % 25 == 0:
-                    await interaction.followup.send(embed=embed)
-                    embed = discord.Embed(
-                    title=f"{interaction.user.name}'s characters"
-                    )
-
-            if j > 25 and (j % 25 != 0):
-                embed.add_field(name=f"{char['name']:10}", value=f"{char['rarity']}* | lvl {char['level']:2} C{char['constellation']}", inline=True)
-        await interaction.followup.send(embed=embed)
+                pages[1].add_field(name=f"{char['name']:10}", value = f"{char['rarity']}* | lvl {char['level']:2} C{char['constellation']}", inline=True)
+            
+        await interaction.response.send_message(embed=pages[0],view = ButtonMenu(pages,60))
+        
     else:
         await interaction.response.send_message("You have to register! (type /register)")
 
@@ -110,11 +113,12 @@ async def characters(interaction: discord.Interaction):
 async def abbys(interaction: discord.Interaction):
     if str(interaction.user.id) in str(db["Users"]["User_ID"]).split("'"):
         stats = gg.abbys(interaction.user.id)
-        embed = discord.Embed(title=f"{interaction.user.name}'s Abbys Stats")
+        embed = discord.Embed
         embed.set_thumbnail(url=interaction.user.avatar.url)
+        embed.set_footer(icon_url = interaction.user.avatar.url ,text=f"Requested by {interaction.user.name}")
         embed.set_image(url="https://static.wikia.nocookie.net/gensin-impact/images/c/ca/Domain_Spiral_Abyss_Abyssal_Moon_Spire.png/revision/latest?cb=20210326011346")
         for field, valuee in stats.items():
-            embed.add_field(name = str(field), value = str(valuee))
+            embed.add_field(name = str(field), value = str(valuee),inline=True)
         await interaction.response.send_message(embed=embed)
     else:
         await interaction.response.send_message("You have to register! (type /register)")
@@ -124,10 +128,11 @@ async def resources(interaction: discord.Interaction):
     if str(interaction.user.id) in str(db["Users"]["User_ID"]).split("'"):
         notes = gg.notes(interaction.user.id)
         embed = discord.Embed(title=f"{interaction.user.name}'s resources")
-        embed.set_thumbnail(url=interaction.user.avatar.url)
+        embed.set_thumbnail(url="https://static.wikia.nocookie.net/gensin-impact/images/3/35/Item_Fragile_Resin.png/revision/latest?cb=20210106074218")
         embed.add_field(name="Current resin: ",value =f"{notes['resin']}/{notes['max_resin']}")
         embed.add_field(name="Current realm currency: ",value =f"{notes['realm_currency']}/{notes['max_realm_currency']}")
         embed.add_field(name="Expeditions: ",value =f"{len(notes['expeditions'])}/{notes['max_expeditions']}")
+        embed.set_footer(icon_url = interaction.user.avatar.url ,text=f"Requested by {interaction.user.name}")
         await interaction.response.send_message(embed=embed)
     else:
         await interaction.response.send_message("You have to register! (type /register)")
